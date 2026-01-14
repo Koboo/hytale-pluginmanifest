ac018a74b4eb7b15a25924f3bb789c817a7e59bd
Koboo <admin@koboo.eu>
2026-01-14 16:49:20 +0100

    Comment in gradle-plugin-publish

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("java-gradle-plugin")
     id("signing")
     id("com.gradleup.shadow") version ("9.3.1")
-    //id("com.gradle.plugin-publish") version("2.0.0")
+    id("com.gradle.plugin-publish") version("2.0.0")
 }
 
 repositories {
