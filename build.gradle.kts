57af74c612b5cae6c71b81d6e378d4eaa521c137
Koboo <admin@koboo.eu>
2026-01-16 21:34:02 +0100

    Update root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,88 +1,91 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar.Companion.shadowJar
+
 plugins {
-    id("java")
     id("maven-publish")
+    id("java-gradle-plugin")
+    id("com.gradle.plugin-publish") version("2.0.0")
     id("com.gradleup.shadow") version ("9.3.1")
 }
 
-subprojects {
-    apply {
-        plugin("java")
-        plugin("maven-publish")
-        plugin("com.gradleup.shadow")
-    }
+group = "eu.koboo"
+version = "1.0.24-rc.1"
 
-    group = "eu.koboo"
-    version = "1.0.24"
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
 
-    repositories {
-        mavenCentral()
-    }
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
 
-    dependencies {
-        compileOnly("org.projectlombok:lombok:1.18.42")
-        annotationProcessor("org.projectlombok:lombok:1.18.42")
-    }
+    implementation("org.jetbrains:annotations:26.0.2-1")
+    implementation("com.github.javaparser:javaparser-core:3.25.9")
+}
 
-    java {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(25))
-        withSourcesJar()
-    }
-
-    tasks {
-        compileJava {
-            options.encoding = "UTF-8"
-        }
-        javadoc {
-            options.encoding = "UTF-8"
-        }
-    }
-
-    sourceSets {
-        main {
-            java.setSrcDirs(listOf("src"))
-            resources.setSrcDirs(emptyList<String>())
-        }
-        test {
-            java.setSrcDirs(emptyList<String>())
-            resources.setSrcDirs(emptyList<String>())
-        }
-    }
-
-    tasks.withType<Javadoc>().configureEach {
-        (options as CoreJavadocOptions).addStringOption("Xdoclint:none", "-quiet")
-    }
-
-    publishing {
-        repositories {
-            mavenLocal()
-            maven {
-                name = "entixReposiliteReleases"
-                url = uri("https://repo.entix.eu/releases")
-                credentials {
-                    username = System.getenv("ENTIX_REPO_USER")
-                    password = System.getenv("ENTIX_REPO_PASS")
-                }
-            }
-            maven {
-                name = "entixReposiliteSnapshots"
-                url = uri("https://repo.entix.eu/snapshots")
-                credentials {
-                    username = System.getenv("ENTIX_REPO_USER")
-                    password = System.getenv("ENTIX_REPO_PASS")
-                }
-            }
+gradlePlugin {
+    plugins {
+        create("pluginManifestPlugin") {
+            id = "eu.koboo.pluginmanifest"
+            displayName = "Hytale Plugin Manifest Generator"
+            description = "Gradle plugin to automatically generate the manifest.json for Hytale plugins."
+            implementationClass = "eu.koboo.pluginmanifest.gradle.plugin.PluginManifestPlugin"
+            website = "https://github.com/Koboo/hytale-pluginmanifest"
+            vcsUrl = "https://github.com/Koboo/hytale-pluginmanifest"
+            tags = setOf("hytale", "manifest", "generator")
         }
     }
 }
 
+tasks.shadowJar {
+    // Required by gradle-publish-plugin
+    archiveClassifier.set("")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+    withSourcesJar()
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    javadoc {
+        options.encoding = "UTF-8"
+        (options as CoreJavadocOptions).addStringOption("Xdoclint:none", "-quiet")
+    }
+}
 
 sourceSets {
     main {
-        java.setSrcDirs(emptyList<String>())
+        java.setSrcDirs(listOf("src"))
         resources.setSrcDirs(emptyList<String>())
     }
     test {
-        java.setSrcDirs(emptyList<String>())
+        java.setSrcDirs(listOf("tests"))
         resources.setSrcDirs(emptyList<String>())
+    }
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+        maven {
+            name = "entixReposiliteReleases"
+            url = uri("https://repo.entix.eu/releases")
+            credentials {
+                username = System.getenv("ENTIX_REPO_USER")
+                password = System.getenv("ENTIX_REPO_PASS")
+            }
+        }
+        maven {
+            name = "entixReposiliteSnapshots"
+            url = uri("https://repo.entix.eu/snapshots")
+            credentials {
+                username = System.getenv("ENTIX_REPO_USER")
+                password = System.getenv("ENTIX_REPO_PASS")
+            }
+        }
     }
 }
