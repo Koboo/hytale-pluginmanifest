5f9e913f21b520ea00fd81c6e088789c55c4001a
Koboo <admin@koboo.eu>
2026-01-16 21:33:07 +0100

    Drop annotation processor support

--- pluginmanifest-api/src/eu/koboo/pluginmanifest/annotations/api/DependencyType.java
+++
@@ -1,6 +0,0 @@
-package eu.koboo.pluginmanifest.annotations.api;
-
-public enum DependencyType {
-
-    REQUIRED, OPTIONAL, LOAD_BEFORE;
-}
--- pluginmanifest-api/src/eu/koboo/pluginmanifest/manifest/validation/ValidationResult.java
+++
@@ -1,8 +0,0 @@
-package eu.koboo.pluginmanifest.manifest.validation;
-
-public record ValidationResult(String key, String value, String message) {
-
-    public static ValidationResult of(String key, String value, String message) {
-        return new ValidationResult(key, value, message);
-    }
-}
